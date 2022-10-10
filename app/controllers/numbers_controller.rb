d768190fb624fd211922e2ea8977530c0d537ac7
Ainsley M Galvez <galvezainsley@gmail.com>
2022-10-10 15:28:15 +0000

    Finished Aries & Leo

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+
+    @unlucky = Array.new
+
+    5.times do
+      number = rand(1...100)
+      @unlucky.push(number)
+    end
+
+    render ({:template => "lottery_stuff/oh_no.html.erb"})
+  end
+
 end
